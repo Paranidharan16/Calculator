0e7007afb6cfe2ae1793eef454bd8e7908739905
Paranidharan16 <paranidharan16@gmail.com>
2023-11-13 14:22:06 +0530

    Changing Code

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,7 @@
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const zero = document.querySelector(".zero");
-
+const btns = document.querySelectorAll(".num");
 let result = document.querySelector(".result");
-
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
 let cal = "";
 
 function calculation(sym) {
@@ -18,69 +9,14 @@ function calculation(sym) {
   result.textContent = cal;
 }
 
-one.addEventListener("click", () => {
-  calculation("1");
-});
-
-two.addEventListener("click", () => {
-  calculation("2");
-});
-three.addEventListener("click", () => {
-  calculation("3");
-});
-four.addEventListener("click", () => {
-  calculation("4");
-});
-
-five.addEventListener("click", () => {
-  calculation("5");
-});
-
-six.addEventListener("click", () => {
-  calculation("6");
-});
-
-seven.addEventListener("click", () => {
-  calculation("7");
-});
-eight.addEventListener("click", () => {
-  calculation("8");
-});
-nine.addEventListener("click", () => {
-  calculation("9");
-});
-
-zero.addEventListener("click", () => {
-  calculation("0");
-});
-
-const dot = document.querySelector(".dot");
-const add = document.querySelector(".add");
-const sub = document.querySelector(".sub");
-const div = document.querySelector(".div");
-const mul = document.querySelector(".mul");
-const clear = document.querySelector(".clear");
-const equal = document.querySelector(".equal");
-
-dot.addEventListener("click", () => {
-  calculation(".");
-});
-add.addEventListener("click", () => {
-  calculation("+");
-});
-sub.addEventListener("click", () => {
-  calculation("-");
-});
-
-mul.addEventListener("click", () => {
-  calculation("*");
-});
-div.addEventListener("click", () => {
-  calculation("/");
+btns.forEach((e) => {
+  e.addEventListener("click", function (e) {
+    calculation(e.target.textContent + "");
+  });
 });
 
 clear.addEventListener("click", () => {
-  cal = "";
+  cal = cal.slice(0, cal.length - 1);
   result.textContent = cal;
 });
 
